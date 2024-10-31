13b5717ebdd87142e6886a44ff91f63cf9769e87
AdamSzwajcowski <adamszwajcowski@gmail.com>
2024-10-31 18:49:33 +0100

    fixed rephrase, summary and thanks

--- responsegenerator.py
+++ responsegenerator.py
@@ -2,7 +2,7 @@ from categorypredictor import CategoryPredictor
 from enum import Enum
 
 # context enum for varying the response based on the previous message
-Context = Enum('Context', ['NONE','SOLODUO','REPHRASE'])
+Context = Enum('Context', ['NONE','SOLODUO'])
 
 class ResponseGenerator:
     
@@ -10,6 +10,7 @@ class ResponseGenerator:
         self.predictor = CategoryPredictor()
         self.soloduo = []  # solo or duo context
         self.context = [Context.NONE]  # general conversational context
+        self.rephrase_counter = 0
         
         
     def check_soloduo(self, sentence):
@@ -28,7 +29,13 @@ class ResponseGenerator:
         return('Hello! How can I help you?')
                
     def summary(self):
-        return("to be fixed")
+        return(" ".join(["I play a wide variety of songs and pieces both solo, as a",
+                "fingerstyle guitarist, and in a duo with singers. This chatbot",
+                "is meant to help you navigate around my repertoire - you can",
+                "ask for a specific song, artist or genre."]))
+    
+    def thanks(self):
+        return("You're welcome! Is there anything else I can do for you?")
     
     def bye(self):
         return('Bye!')
@@ -37,12 +44,13 @@ class ResponseGenerator:
         '''
         Punt if there are two misunderstandings in a row. Otherwise, ask for rephrasing.
         '''
-        if self.context[0] == Context.REPHRASE:
+        if self.rephrase_counter:
             return("I'm afraid I can't help you with that. Can I do anything else for you?")
-            self.context = [Context.NONE]           
+            self.context = [Context.NONE]
+            self.rephrase_counter = 0
         else:
             return("I couldn't quite understand, could you rephrase?")
-            self.context.insert(0,Context.REPHRASE)
+            self.rephrase_counter += 1
             
         
 
@@ -50,11 +58,14 @@ class ResponseGenerator:
         """
         Perform an action based on the recognized category.
         """
+        self.rephrase_counter = 0
         self.context = [Context.NONE]    # any previous context becomes irrelevant
         if category == self.predictor.categories.GREETINGS:
             return(self.greetings())
         elif category == self.predictor.categories.SUMMARY:
             return(self.summary())
+        elif category == self.predictor.categories.THANKS:
+            return(self.thanks())
         elif category == self.predictor.categories.BYE:
             return(self.bye())
         else:  # REPERTOIRE, SONG_NAME, ARTIST_NAME or GENRE_NAME
@@ -71,21 +82,8 @@ class ResponseGenerator:
         Return a response based on the input sentence (prompt).
         """
         print(self.context)
-        if self.context[0] == Context.SOLODUO:              
-            self.check_soloduo(sentence)               
-            # check if some other request was made despite only asking for clarification
-            category, probability, proper_name = self.predictor.predict(sentence)
-            print(category, probability)
-            if probability > 0.95:                   
-                return(self.take_action(category, proper_name))                     
-            elif not self.soloduo:   # if the user still hasn't specified solo/duo
-                return(self.misunderstand())
-            else:   # if the user simply provided solo/duo
-                # read the category and proper_name from the context after SOLODUO
-                category, proper_name = self.context[1]
-                return(self.take_action(category, proper_name))
                     
-        elif self.context[0] == Context.NONE:
+        if self.context[0] == Context.NONE:
             self.check_soloduo(sentence)
             category, probability, proper_name = self.predictor.predict(sentence)
             print(category, probability)
@@ -95,10 +93,24 @@ class ResponseGenerator:
                 #przeszukaj_baze # przeszukiwanie baz
                 if False:   # if znalezione
                     return(1)
-                elif probability > 0.7: # less certain, but still quite probable
+                elif probability > 0.8: # less certain, but still quite probable
                     return(self.take_action(category, proper_name))               
                 else:
                     return(self.misunderstand())
+                
+        elif self.context[0] == Context.SOLODUO:              
+            self.check_soloduo(sentence)               
+            # check if some other request was made despite only asking for clarification
+            category, probability, proper_name = self.predictor.predict(sentence)
+            print(category, probability)
+            if probability > 0.95:                   
+                return(self.take_action(category, proper_name))                     
+            elif not self.soloduo:   # if the user still hasn't specified solo/duo
+                return(self.misunderstand())
+            else:   # if the user simply provided solo/duo
+                # come back to the previously saved request
+                category, proper_name = self.context[1]
+                return(self.take_action(category, proper_name))
                 
 
 # test
